f071c3f3e441d2a75c680d6c903b6ec8fe9a4443
mbpz <apples398@163.com>
2026-05-02 20:02:43 +0800

    chore: update Homebrew formula with real SHA256

--- Formula/proxybot-tui.rb
+++ Formula/proxybot-tui.rb
@@ -6,10 +6,10 @@ class ProxybotTui < Formula
 
   if OS.mac? && Hardware::CPU.type == :arm64
     url "https://github.com/mbpz/proxybot/releases/download/tui-v0.4.2/proxybot-tui-arm64"
-    sha256 ""
+    sha256 "f23af267022294d9482b7036c83761163eac3e8165452c63892d9f0563e9cca8"
   elsif OS.mac? && Hardware::CPU.type == :intel
     url "https://github.com/mbpz/proxybot/releases/download/tui-v0.4.2/proxybot-tui-x86_64"
-    sha256 ""
+    sha256 "07df5fd82d239232e52fba318ad295c82452c360e166f3297a5cfce9a80fdff5"
   end
 
   def install
