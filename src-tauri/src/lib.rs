86c7a478b2eec29bb5bacb3b943e513b221eec48
mbpz <apples398@163.com>
2026-05-04 09:14:11 +0800

    chore: lockfile and config updates for Yew GUI

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -31,6 +31,7 @@ pub mod vision;
 pub mod tui;
 pub mod update_check;
 pub mod adb;
+pub mod gui;
 
 use anomaly::{AnomalyDetector, get_alerts, acknowledge_alert, get_alert_count, get_traffic_baseline, scan_request_anomalies};
 use cert::CertManager;
